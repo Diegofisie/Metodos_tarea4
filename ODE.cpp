ef6d4e8f70aba90e059d5e30aa33887193ce05c5
Diego Martinez <df.martinezv@uniandes.edu.co>
2018-11-12 21:25:39 -0500

    Prueba

---
+++ ODE.cpp
@@ -0,0 +1,10 @@
+/* En este ejercicio deben solucionar la ecuacion de movimiento de un proyectil para el caso en
+que hay una friccion proporcional a v2. Deben escribir un codigo en c++ llamado ODE cpp donde implementen el metodo de Leap-Frog o el metodo de Runge Kutta de 4to orden para
+resolver la ecuacon de movimiento:*/
+#include <iostream>
+#include <cmath>
+
+int main() {
+    
+  return 0;
+}
